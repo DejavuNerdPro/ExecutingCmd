5a44c74204d358820be4f4dca09445aa9b4b1c1d
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2024-01-12 11:25:18 +0630

    runtimeProcessExecutor

--- src/CmdExecutor.java
+++ src/CmdExecutor.java
@@ -1,5 +1,42 @@
 package src;
 
+import java.io.IOException;
+
 public class CmdExecutor {
+
+    private static CmdExecutor instance;
+
+    public CmdExecutor() {
+    }
+
+    // Singleton
+    public static CmdExecutor getInstance() {
+        if (instance == null) {
+            instance = new CmdExecutor();
+        }
+
+        return instance;
+    }
+
+    // for using java.lang.Runtime
+    public static int runtimeProcessExecutor(String cmd) {
+
+        Process runtimeProcess = null;
+        try {
+            runtimeProcess = Runtime.getRuntime().exec(cmd);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            System.out.println(ioe);
+        }
+
+        int runtimeComplete = 0;
+        try {
+            runtimeComplete = runtimeProcess.waitFor();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return runtimeComplete;
+    }
 
 }
